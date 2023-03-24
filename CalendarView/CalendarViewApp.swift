4aa11a52c1bc2690d9c6a67d0a0c79eb688e1aea
Philipp <pd95@users.noreply.github.com>
2023-03-24 16:22:55 +0100

    Initial Commit

---
+++ CalendarView/CalendarViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarViewApp.swift
+//  CalendarView
+//
+//  Created by Philipp on 24.03.23.
+//
+
+import SwiftUI
+
+@main
+struct CalendarViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
