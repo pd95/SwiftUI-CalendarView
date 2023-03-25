7af864af621ac935e6d6f07747cec67a5e4e0e45
Philipp <pd95@users.noreply.github.com>
2023-03-25 14:23:26 +0100

    Make date non-optional to simplify code

--- CalendarView/ContentView.swift
+++ CalendarView/ContentView.swift
@@ -11,21 +11,16 @@ struct ContentView: View {
     @Environment(\.calendar) private var calendar
     @State private var minDate = Date.distantPast
     @State private var maxDate = Date.distantFuture
-    @State private var date: Date?
+    @State private var date: Date = .now
+    @State private var locale: Locale = .current
     @SceneStorage("maxWidth") private var maxWidth: Int = 350
     @SceneStorage("maxHeight") private var maxHeight: Int = 320
-    @State private var locale: Locale = .current
-
-    var dateComponents: DateComponents? {
-        guard let date else { return nil }
-        return calendar.dateComponents([.year, .month, .day], from: date)
-    }
 
     var body: some View {
         VStack(alignment: .center, spacing: 20) {
             ScrollView(.vertical) {
                 CalendarView(
-                    visibleDateComponents: dateComponents,
+                    visibleDateComponents: calendar.dateComponents([.year, .month, .day], from: date),
                     availableDateRange: DateInterval(start: minDate, end: maxDate)
                 )
                 .environment(\.locale, locale)
@@ -39,7 +34,7 @@ struct ContentView: View {
             Section {
                 LabeledContent("Month") {
                     Button(action: {
-                        date = calendar.date(byAdding: .month, value: -1, to: date ?? .now)
+                        date = calendar.date(byAdding: .month, value: -1, to: date) ?? date
                     }, label: {
                         Label("Previous", systemImage: "chevron.backward.2")
                     })
@@ -47,7 +42,7 @@ struct ContentView: View {
                         date = .now
                     }
                     Button(action: {
-                        date = calendar.date(byAdding: .month, value: 1, to: date ?? .now)
+                        date = calendar.date(byAdding: .month, value: 1, to: date) ?? date
                     }, label: {
                         Label("Next", systemImage: "chevron.forward.2")
                     })
